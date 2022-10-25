1f5b88b96ef575817a8b6d44981c89b89f492b10
Andy Friedman <afriedman412@gmail.com>
2022-10-24 22:09:06 -0400

    added seed access, fixed type error

--- app.py
+++ app.py
@@ -6,19 +6,18 @@ app = Flask(__name__)
 load_dotenv()
 squares = open("static/squares.txt").readlines()
 
-def make_squares(seed=None, squares=squares):
-    if seed:
-        random.seed(seed)
-    selections = random.sample(squares, k=24)
+def make_squares(seed, squares=squares):
+    selections = random.Random(seed).sample(squares, k=24)
     selections.insert(12, "LIGHTS OUT and AWAY WE GO!!!")
     return selections
 
+@app.route('/bingo/')
 @app.route('/')
-@app.route('/<seed>')
+@app.route('/bingo/<seed>')
 def home(seed=None):
     if not seed:
         seed = random.randrange(100, 1000000000)
-    select_squares = make_squares(seed)
+    select_squares = make_squares(str(seed))
     return render_template("home.html", seed=seed, squares=select_squares)
     
 if __name__ == '__main__':
